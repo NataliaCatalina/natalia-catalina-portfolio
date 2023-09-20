b8acfdadadfaee56a7c20bd8564d4d14e003afd4
nataliacatalina <ntlc.ramirez@gmail.com>
2023-09-20 16:34:19 +1200

    delete option photo retouch

--- js/script.js
+++ js/script.js
@@ -1,25 +1,23 @@
-const portfolioNavLinks = document.querySelectorAll('#projects .nav-link');
-const portfolioItems = document.querySelectorAll('.portfolio');
+const portfolioNavLinks = document.querySelectorAll('#projects .nav-link')
+const portfolioItems = document.querySelectorAll('.portfolio')
 
-portfolioNavLinks.forEach(link => {
-  link.addEventListener('click', function(event) {
-    const selectedCategory = this.getAttribute('data-category');
+portfolioNavLinks.forEach((link) => {
+  link.addEventListener('click', function (event) {
+    const selectedCategory = this.getAttribute('data-category')
 
-    portfolioNavLinks.forEach(link => link.classList.remove('active'));
+    portfolioNavLinks.forEach((link) => link.classList.remove('active'))
 
-    if (selectedCategory !== 'photosretouch') {
-      event.preventDefault();
-    }
+    event.preventDefault()
 
-    this.classList.add('active');
+    this.classList.add('active')
 
-    portfolioItems.forEach(item => {
-      const itemCategory = item.getAttribute('data-category');
-      if (selectedCategory === 'all' || itemCategory === selectedCategory || selectedCategory === 'photosretouch') {
-        item.style.display = 'block';
+    portfolioItems.forEach((item) => {
+      const itemCategory = item.getAttribute('data-category')
+      if (selectedCategory === 'all' || itemCategory === selectedCategory) {
+        item.style.display = 'block'
       } else {
-        item.style.display = 'none';
+        item.style.display = 'none'
       }
-    });
-  });
-});
+    })
+  })
+})
