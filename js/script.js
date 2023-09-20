cc659a1ec193081d2ce56c273604645d4b82d518
nataliacatalina <ntlc.ramirez@gmail.com>
2023-09-20 14:38:29 +1200

    delete photo retouch

--- js/script.js
+++ js/script.js
@@ -3,22 +3,21 @@ const portfolioItems = document.querySelectorAll('.portfolio');
 
 portfolioNavLinks.forEach(link => {
   link.addEventListener('click', function(event) {
-    const selectedCategory = this.getAttribute('data-category');
+
+    event.preventDefault();
 
     portfolioNavLinks.forEach(link => link.classList.remove('active'));
 
-    if (selectedCategory !== 'photosretouch') {
-      event.preventDefault();
-    }
-
     this.classList.add('active');
+
+    const selectedCategory = this.getAttribute('data-category');
 
     portfolioItems.forEach(item => {
       const itemCategory = item.getAttribute('data-category');
-      if (selectedCategory === 'all' || itemCategory === selectedCategory || selectedCategory === 'photosretouch') {
-        item.style.display = 'block';
+      if (selectedCategory === 'all' || itemCategory === selectedCategory) {
+        item.style.display = 'block'; 
       } else {
-        item.style.display = 'none';
+        item.style.display = 'none'; 
       }
     });
   });
