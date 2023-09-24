a76b62dc16930e0996accf4cedca47dd8824eb1b
nataliacatalina <ntlc.ramirez@gmail.com>
2023-09-24 16:29:39 +1300

    add photo retouch

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
-const portfolioNavLinks = document.querySelectorAll('#projects .nav-link')
-const portfolioItems = document.querySelectorAll('.portfolio')
+document.addEventListener('DOMContentLoaded', function () {
+  const portfolioNavLinks = document.querySelectorAll('#projects .nav-link')
+  const portfolioItems = document.querySelectorAll('.portfolio-item')
 
-portfolioNavLinks.forEach((link) => {
-  link.addEventListener('click', function (event) {
-    const selectedCategory = this.getAttribute('data-category')
+  portfolioNavLinks.forEach((link) => {
+    link.addEventListener('click', function (event) {
+      const selectedCategory = this.getAttribute('data-category')
 
-    portfolioNavLinks.forEach((link) => link.classList.remove('active'))
+      portfolioNavLinks.forEach((link) => link.classList.remove('active'))
 
-    event.preventDefault()
+      event.preventDefault()
 
-    this.classList.add('active')
+      this.classList.add('active')
 
-    portfolioItems.forEach((item) => {
-      const itemCategory = item.getAttribute('data-category')
-      if (selectedCategory === 'all' || itemCategory === selectedCategory) {
-        item.style.display = 'block'
-      } else {
-        item.style.display = 'none'
-      }
+      portfolioItems.forEach((item) => {
+        const itemCategory = item.getAttribute('data-category')
+        if (selectedCategory === 'all' || itemCategory === selectedCategory) {
+          item.style.display = 'block'
+        } else {
+          item.style.display = 'none'
+        }
+      })
     })
   })
 })
